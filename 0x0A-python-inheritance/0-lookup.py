ae0f4e4e6fe2a3964b7125472a87386b2d2f1952
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-11-07 02:02:30 +0200

    Done with first task, and create files for rest of tasks

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A module for getting available attributes and methods."""
+
+
+def lookup(obj):
+    """Look up for available attributes and methods of an object."""
+    return dir(obj)
