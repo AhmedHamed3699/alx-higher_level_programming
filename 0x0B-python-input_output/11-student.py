13ea8ea61cf1d1b66e48488c1f470523739a86da
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-11-09 20:06:43 +0200

    Solved most of the main tasks

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""A module for studen class."""
+
+
+class Student:
+    """Class for student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Construct a new object."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Convert class to json dictionary."""
+        my_dict = self.__dict__
+        if isinstance(attrs, list):
+            new_dict = {}
+            for attr in attrs:
+                if not isinstance(attr, str):
+                    return my_dict
+                if my_dict.get(attr):
+                    new_dict[attr] = my_dict[attr]
+            del my_dict
+            my_dict = new_dict
+        return my_dict
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student."""
+        for key, value in json.items():
+            if key in self.__dict__:
+                self.__dict__[key] = value
