ee22af7ce0c5d21c492ee378263d3a7961d1a05d
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2023-11-14 02:39:14 +0200

    Done with main tasks

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Base Module."""
+import json
+from pathlib import Path
 
 
 class Base:
@@ -14,3 +16,56 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Convert list_dictionaries to json."""
+        if list_dictionaries is None:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Save objects to file."""
+        saved_list = []
+        for obj in list_objs:
+            saved_list.append(obj.to_dictionary())
+        with open(f'{cls.__name__}.json', 'w', encoding='utf-8') as file:
+            file.write(cls.to_json_string(saved_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Convert json string to list of dictionaries."""
+        if not json_string:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Create a new instance."""
+        from models.rectangle import Rectangle
+        from models.square import Square
+        inst = None
+        if cls is Rectangle:
+            inst = Rectangle(1, 1)
+        elif cls is Square:
+            inst = Square(1)
+        inst.update(**dictionary)
+        return inst
+
+    @classmethod
+    def load_from_file(cls):
+        """Load objects from file."""
+        file_name = f'{cls.__name__}.json'
+        file_path = Path(file_name)
+        if not file_path.is_file():
+            return []
+        obj_list = []
+        json_dict = ""
+        with open(file_name, 'r', encoding='utf-8') as file:
+            json_dict = file.read()
+        obj_list_dict = cls.from_json_string(json_dict)
+        for obj_dict in obj_list_dict:
+            new_obj = cls.create(**obj_dict)
+            obj_list.append(new_obj)
+        return obj_list
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Square module."""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class, that inherits from Rectangle."""
+
+    def __init__(self, size, x=0, y=0, id_num=None):
+        """Construct Square object."""
+        super().__init__(size, size, x, y, id_num)
+
+    def __str__(self):
+        """Convert object to string."""
+        return f'[Square] ({self.id}) {self.x}/{self.y} - {self.width}'
+
+    @property
+    def size(self):
+        """Get size."""
+        return self.width
+
+    @size.setter
+    def size(self, val):
+        """Set size."""
+        self.width = val
+        self.height = val
+
+    def update(self, *args, **kwargs):
+        """Update Square."""
+        if not args:
+            if not kwargs:
+                return
+
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
+        else:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+
+    def to_dictionary(self):
+        """Convert object to dictionary."""
+        new_dict = super().to_dictionary()
+        new_dict.pop('height')
+        new_dict['size'] = new_dict.pop('width')
+        return new_dict
