a3b4ddb79461ccedb24ef1414461803f3d16d7a1
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-01-16 17:03:09 +0200

    prepare files

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,3 @@
+-- Create a database
+-- Query to create the database hbtn_0c_0 if not exist
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,3 @@
+-- Delete a database
+-- Query to drop hbtn_0c_0 database if exist
+DROP DATABASE IF EXISTS hbtn_0c_0;
