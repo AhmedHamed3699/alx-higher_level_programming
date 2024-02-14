f92134dfbe36da9c01bc6deff6d21884b01dfad8
AhmedHamed3699 <Ahmed.Hamed03@eng-st.cu.edu.eg>
2024-02-14 20:38:12 +0200

    Modify last task

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  const digits = '0123456789abcdef';
   return function (number) {
-    let ans = '';
-    while (number) {
-      ans = digits[number % base].toString() + ans;
-      number = parseInt(number / base);
-    }
-    return ans;
+    return number.toString(base);
   };
 };
